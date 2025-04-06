b9207d2ee48ede331db0891fe706b174b53ae594
Anwer Khan <deepwatrcreatur@tarpit.deepwatercreature.com>
2025-04-06 16:25:01 -0400

    changed path to metrics file, use grafana alloy to push instead of curl in temp reading script

--- drive_temp.sh
+++ drive_temp.sh
@@ -4,7 +4,7 @@
 drives="sdc sdd sde sdf"
 
 # File to store metrics temporarily
-metrics_file="/tmp/drive_temp_metrics.prom"
+metrics_file="/var/lib/drive_temp_metrics.prom"
 
 # Clear the file
 > "$metrics_file"
@@ -16,11 +16,3 @@ for drive in $drives; do
     echo "drive_temperature_celsius{drive=\"/dev/$drive\"} $temp" >> "$metrics_file"
 done
 
-# Push metrics to Grafana Cloud
-GRAFANA_CLOUD_URL="https://deepwatrcreatur.grafana.net" # Replace with your URL
-GRAFANA_CLOUD_USER="deepwatrcreatur"  # Replace with your Grafana Cloud Prometheus user ID
-GRAFANA_CLOUD_API_KEY="insert key here"  # Replace with your Grafana Cloud API key
-
-curl -u "$GRAFANA_CLOUD_USER:$GRAFANA_CLOUD_API_KEY" \
-     --data-binary "@$metrics_file" \
-     "$GRAFANA_CLOUD_URL"
